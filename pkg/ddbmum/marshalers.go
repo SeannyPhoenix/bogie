0c0b38345a74cf1fff18df1c00683af8bb95f06f
Seanny Phoenix <seannyphoenix@gmail.com>
2025-01-20 02:52:27 +0000

    add list (slice, array), map, struct

--- pkg/ddbmum/marshalers.go
+++ pkg/ddbmum/marshalers.go
@@ -7,8 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-func marshalString(v reflect.Value) (types.AttributeValue, error) {
-	return &types.AttributeValueMemberS{Value: v.String()}, nil
+func marshalBool(v reflect.Value) (types.AttributeValue, error) {
+	return &types.AttributeValueMemberBOOL{Value: v.Bool()}, nil
 }
 
 func marshalInt(v reflect.Value) (types.AttributeValue, error) {
@@ -25,8 +25,36 @@ func marshalFloatFactory(size int) marshaler {
 	}
 }
 
-func marshalBool(v reflect.Value) (types.AttributeValue, error) {
-	return &types.AttributeValueMemberBOOL{Value: v.Bool()}, nil
+func marshalList(v reflect.Value) (types.AttributeValue, error) {
+	l := v.Len()
+	values := make([]types.AttributeValue, l)
+
+	for i := 0; i < l; i++ {
+		value, err := marshalValue(v.Index(i))
+		if err != nil {
+			return nil, err
+		}
+
+		values[i] = value
+	}
+
+	return &types.AttributeValueMemberL{Value: values}, nil
+}
+
+func marshalMap(v reflect.Value) (types.AttributeValue, error) {
+	keys := v.MapKeys()
+	values := make(map[string]types.AttributeValue, len(keys))
+
+	for _, key := range keys {
+		value, err := marshalValue(v.MapIndex(key))
+		if err != nil {
+			return nil, err
+		}
+
+		values[key.String()] = value
+	}
+
+	return &types.AttributeValueMemberM{Value: values}, nil
 }
 
 func marshalPointer(v reflect.Value) (types.AttributeValue, error) {
@@ -35,6 +63,27 @@ func marshalPointer(v reflect.Value) (types.AttributeValue, error) {
 	}
 
 	return marshalValue(v.Elem())
+}
+
+func marshalString(v reflect.Value) (types.AttributeValue, error) {
+	return &types.AttributeValueMemberS{Value: v.String()}, nil
+}
+
+func marshalStruct(v reflect.Value) (types.AttributeValue, error) {
+	fields := v.NumField()
+	values := make(map[string]types.AttributeValue, fields)
+
+	for i := 0; i < fields; i++ {
+		field := v.Type().Field(i)
+		value, err := marshalValue(v.Field(i))
+		if err != nil {
+			return nil, err
+		}
+
+		values[field.Name] = value
+	}
+
+	return &types.AttributeValueMemberM{Value: values}, nil
 }
 
 func marshalValue(v reflect.Value) (types.AttributeValue, error) {
@@ -53,7 +102,7 @@ var defaultMarshalers = map[reflect.Kind]marshaler{
 
 func init() {
 	defaultMarshalers = map[reflect.Kind]marshaler{
-		reflect.String:  marshalString,
+		reflect.Bool:    marshalBool,
 		reflect.Int:     marshalInt,
 		reflect.Int8:    marshalInt,
 		reflect.Int16:   marshalInt,
@@ -66,7 +115,11 @@ func init() {
 		reflect.Uint64:  marshalUint,
 		reflect.Float32: marshalFloatFactory(32),
 		reflect.Float64: marshalFloatFactory(64),
-		reflect.Bool:    marshalBool,
+		reflect.Array:   marshalList,
+		reflect.Map:     marshalMap,
 		reflect.Pointer: marshalPointer,
+		reflect.Slice:   marshalList,
+		reflect.String:  marshalString,
+		reflect.Struct:  marshalStruct,
 	}
 }
--- pkg/ddbmum/marshalers_test.go
+++ pkg/ddbmum/marshalers_test.go
@@ -18,23 +18,49 @@ func TestMarshalValue(t *testing.T) {
 		expected types.AttributeValue
 		err      error
 	}{
-		"string":      {"string", &types.AttributeValueMemberS{Value: "string"}, nil},
-		"int":         {27, &types.AttributeValueMemberN{Value: "27"}, nil},
-		"int8":        {int8(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"int16":       {int16(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"int32":       {int32(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"int64":       {int64(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"uint":        {uint(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"uint8":       {uint8(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"uint16":      {uint16(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"uint32":      {uint32(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"uint64":      {uint64(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"float32":     {float32(3.14), &types.AttributeValueMemberN{Value: "3.14"}, nil},
-		"float64":     {3.14, &types.AttributeValueMemberN{Value: "3.14"}, nil},
-		"bool":        {true, &types.AttributeValueMemberBOOL{Value: true}, nil},
-		"pointer":     {util.Ptr(27), &types.AttributeValueMemberN{Value: "27"}, nil},
-		"pointerNil":  {util.NilPtr[int](), nil, nil},
-		"unsupported": {struct{}{}, nil, errors.New("unsupported type: struct")},
+		"bool":       {true, &types.AttributeValueMemberBOOL{Value: true}, nil},
+		"int":        {27, &types.AttributeValueMemberN{Value: "27"}, nil},
+		"int8":       {int8(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"int16":      {int16(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"int32":      {int32(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"int64":      {int64(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"uint":       {uint(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"uint8":      {uint8(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"uint16":     {uint16(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"uint32":     {uint32(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"uint64":     {uint64(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"float32":    {float32(3.14), &types.AttributeValueMemberN{Value: "3.14"}, nil},
+		"float64":    {3.14, &types.AttributeValueMemberN{Value: "3.14"}, nil},
+		"complex64":  {complex64(3.14 + 2.71i), nil, errors.New("unsupported type: complex64")},
+		"complex128": {3.14 + 2.71i, nil, errors.New("unsupported type: complex128")},
+		"array": {[3]string{"a", "b", "c"}, &types.AttributeValueMemberL{Value: []types.AttributeValue{
+			&types.AttributeValueMemberS{Value: "a"},
+			&types.AttributeValueMemberS{Value: "b"},
+			&types.AttributeValueMemberS{Value: "c"},
+		}}, nil},
+		"chan": {make(chan int), nil, errors.New("unsupported type: chan")},
+		"func": {func() {}, nil, errors.New("unsupported type: func")},
+		// "interface": {interface{}(27), nil, errors.New("unsupported type: interface")},
+		"map": {map[string]int{"a": 1, "b": 2}, &types.AttributeValueMemberM{Value: map[string]types.AttributeValue{
+			"a": &types.AttributeValueMemberN{Value: "1"},
+			"b": &types.AttributeValueMemberN{Value: "2"},
+		}}, nil},
+		"pointer":    {util.Ptr(27), &types.AttributeValueMemberN{Value: "27"}, nil},
+		"pointerNil": {util.NilPtr[int](), nil, nil},
+		"slice": {[]int{1, 2, 3}, &types.AttributeValueMemberL{Value: []types.AttributeValue{
+			&types.AttributeValueMemberN{Value: "1"},
+			&types.AttributeValueMemberN{Value: "2"},
+			&types.AttributeValueMemberN{Value: "3"},
+		}}, nil},
+		"string": {"string", &types.AttributeValueMemberS{Value: "string"}, nil},
+		"struct": {struct {
+			A int
+			B bool
+		}{27, true}, &types.AttributeValueMemberM{Value: map[string]types.AttributeValue{
+			"A": &types.AttributeValueMemberN{Value: "27"},
+			"B": &types.AttributeValueMemberBOOL{Value: true},
+		}}, nil},
+		// "unsafePointer": {util.UnsafePtr(27), nil, errors.New("unsupported type: unsafe.Pointer")},
 	}
 
 	for name, tc := range tt {
@@ -47,6 +73,8 @@ func TestMarshalValue(t *testing.T) {
 			v, err := marshalValue(reflect.ValueOf(tc.field))
 
 			if tc.err != nil {
+				var e *UnsupportedTypeError
+				assert.ErrorAs(err, &e)
 				assert.EqualError(err, tc.err.Error())
 			} else {
 				assert.NoError(err)
