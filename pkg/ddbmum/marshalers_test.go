8e9925e76b9466274305f02aca7684ca5417f278
Seanny Phoenix <seannyphoenix@gmail.com>
2025-01-20 03:59:00 +0000

    handle BinaryMarshaler

--- pkg/ddbmum/marshalers_test.go
+++ pkg/ddbmum/marshalers_test.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"reflect"
 	"testing"
+	"unsafe"
 
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/google/uuid"
 	"github.com/seannyphoenix/bogie/pkg/util"
 	"github.com/stretchr/testify/assert"
 )
@@ -40,10 +42,14 @@ func TestMarshalValue(t *testing.T) {
 		}}, nil},
 		"chan": {make(chan int), nil, errors.New("unsupported type: chan")},
 		"func": {func() {}, nil, errors.New("unsupported type: func")},
-		// "interface": {interface{}(27), nil, errors.New("unsupported type: interface")},
+		// "interface"
 		"map": {map[string]int{"a": 1, "b": 2}, &types.AttributeValueMemberM{Value: map[string]types.AttributeValue{
 			"a": &types.AttributeValueMemberN{Value: "1"},
 			"b": &types.AttributeValueMemberN{Value: "2"},
+		}}, nil},
+		"mapIntKey": {map[int]string{1: "a", 2: "b"}, &types.AttributeValueMemberM{Value: map[string]types.AttributeValue{
+			"1": &types.AttributeValueMemberS{Value: "a"},
+			"2": &types.AttributeValueMemberS{Value: "b"},
 		}}, nil},
 		"pointer":    {util.Ptr(27), &types.AttributeValueMemberN{Value: "27"}, nil},
 		"pointerNil": {util.NilPtr[int](), nil, nil},
@@ -60,7 +66,8 @@ func TestMarshalValue(t *testing.T) {
 			"A": &types.AttributeValueMemberN{Value: "27"},
 			"B": &types.AttributeValueMemberBOOL{Value: true},
 		}}, nil},
-		// "unsafePointer": {util.UnsafePtr(27), nil, errors.New("unsupported type: unsafe.Pointer")},
+		"unsafePointer":   {unsafe.Pointer(util.Ptr(27)), nil, errors.New("unsupported type: unsafe.Pointer")},
+		"binaryMarshaler": {uuid.MustParse("12db069a-c8dc-4a5a-af0c-22282ed89f3c"), &types.AttributeValueMemberB{Value: []byte{0x12, 0xdb, 0x06, 0x9a, 0xc8, 0xdc, 0x4a, 0x5a, 0xaf, 0x0c, 0x22, 0x28, 0x2e, 0xd8, 0x9f, 0x3c}}, nil},
 	}
 
 	for name, tc := range tt {
